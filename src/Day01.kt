dd10d51a0603868e42cfc3ef23163f5b257f5258
luisdt <luis.ernesto951008@gmail.com>
2021-12-06 16:31:03 -0300

    Finish day 1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
+    fun calculation(input: List<Int>): Int {
+        var previousMeasurement: Int? = null
+        var count = 0
+
+        input.forEach { currentMeasurement ->
+            previousMeasurement?.let { prev ->
+                if (currentMeasurement > prev){
+                    count += 1
+                }
+            }
+
+            previousMeasurement = currentMeasurement
+        }
+
+        return count
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return calculation(input.map { s -> s.toInt() })
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val chunkedInput = input.windowed(3) {
+            it.sumOf { s -> s.toInt() }
+        }
+
+        return calculation(chunkedInput)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
