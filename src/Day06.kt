e10f144f05fc81fe2a201fd5635e5e9c242baec7
luisdt <luis.ernesto951008@gmail.com>
2021-12-13 17:06:07 -0300

    Finish day 6 part 1

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,19 +1,53 @@
-fun main() {
-    fun part1(input: List<String>, days: Int): Int {
-        return 0
+const val FISH_INITIAL_INTERNAL_COUNTER = 8
+
+fun parseFishesState(input: List<String>): List<Int> {
+    return input.first().split(",").map { it.toInt() }
+}
+
+fun simulateFishesGrowthRecursive(initialFishesState: List<Int>, days: Long): Long {
+    if (days <= 0) {
+        return initialFishesState.size.toLong()
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    var newFishes = 0
+
+    val nextFishesState = initialFishesState.map {
+        var result = it - 1
+
+        if (result < 0) {
+            result = 6
+            newFishes += 1
+        }
+
+        result
+    }.toMutableList()
+
+    repeat(newFishes) {
+        nextFishesState.add(FISH_INITIAL_INTERNAL_COUNTER)
     }
-    
+
+    return simulateFishesGrowthRecursive(nextFishesState, days - 1)
+}
+
+
+fun main() {
+    fun part1(input: List<String>, days: Long): Long {
+        val initialFishesState = parseFishesState(input)
+        return simulateFishesGrowthRecursive(initialFishesState, days)
+    }
+
+    fun part2(input: List<String>, days: Long): Long {
+        val initialFishesState = parseFishesState(input)
+        return simulateFishesGrowthRecursive(initialFishesState, days)
+    }
+
     val testInput = readInput("Day06_test")
-    check(part1(testInput, 18) == 26)
-    check(part1(testInput, 80) == 5934)
-//    check(part2(testInput) == 12)
+    check(part1(testInput, 18) == 26L)
+    check(part1(testInput, 80) == 5934L)
+//    check(part2(testInput, 256) == 26984457539L)
 
     val input = readInput("Day06")
-    println(part1(input, 0))
-//    println(part2(input))
+    println(part1(input, 80))
+//    println(part2(input, 256))
 
 }
