3719ac26438fc2464053519eaa85f2fab6c0e6b4
luisdt <luis.ernesto951008@gmail.com>
2021-12-15 19:17:27 -0300

    Finish day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,53 +1,45 @@
-const val FISH_INITIAL_INTERNAL_COUNTER = 8
+const val CURRENT_FISH_INTERNAL_CLOCK = 6
+const val NEW_FISH_INTERNAL_CLOCK = 8
 
-fun parseFishesState(input: List<String>): List<Int> {
-    return input.first().split(",").map { it.toInt() }
+fun parseFishesState(input: List<String>): IntArray {
+    return input.first().split(",").map { it.toInt() }.toIntArray()
 }
 
-fun simulateFishesGrowthRecursive(initialFishesState: List<Int>, days: Long): Long {
-    if (days <= 0) {
-        return initialFishesState.size.toLong()
-    }
+fun simulateFishesGrowthRate(initialFishesState: IntArray, lastGenerationDay: Int): Long {
+    val newFishesByDate = LongArray(lastGenerationDay + 1) { 0 }
 
-    var newFishes = 0
+    initialFishesState.forEach { currentFishAge -> ++newFishesByDate[currentFishAge + 1] }
 
-    val nextFishesState = initialFishesState.map {
-        var result = it - 1
+    (0 .. lastGenerationDay).forEach { day ->
+        val nextDayToPopulateInitial = day + CURRENT_FISH_INTERNAL_CLOCK + 1
+        val nextDayToPopulateNew = day + NEW_FISH_INTERNAL_CLOCK + 1
+        val currentFishesAtDay = newFishesByDate[day]
 
-        if (result < 0) {
-            result = 6
-            newFishes += 1
+        if (nextDayToPopulateNew < newFishesByDate.size){
+            newFishesByDate[nextDayToPopulateNew] += currentFishesAtDay
         }
 
-        result
-    }.toMutableList()
-
-    repeat(newFishes) {
-        nextFishesState.add(FISH_INITIAL_INTERNAL_COUNTER)
+        if (nextDayToPopulateInitial < newFishesByDate.size){
+            newFishesByDate[nextDayToPopulateInitial] += currentFishesAtDay
+        }
     }
 
-    return simulateFishesGrowthRecursive(nextFishesState, days - 1)
+    return newFishesByDate.sum() + initialFishesState.size
 }
 
 
 fun main() {
-    fun part1(input: List<String>, days: Long): Long {
+    fun part1and2(input: List<String>, days: Int): Long {
         val initialFishesState = parseFishesState(input)
-        return simulateFishesGrowthRecursive(initialFishesState, days)
-    }
-
-    fun part2(input: List<String>, days: Long): Long {
-        val initialFishesState = parseFishesState(input)
-        return simulateFishesGrowthRecursive(initialFishesState, days)
+        return simulateFishesGrowthRate(initialFishesState, days)
     }
 
     val testInput = readInput("Day06_test")
-    check(part1(testInput, 18) == 26L)
-    check(part1(testInput, 80) == 5934L)
-//    check(part2(testInput, 256) == 26984457539L)
+    check(part1and2(testInput, 18) == 26L)
+    check(part1and2(testInput, 80) == 5934L)
+    check(part1and2(testInput, 256) == 26_984_457_539L)
 
     val input = readInput("Day06")
-    println(part1(input, 80))
-//    println(part2(input, 256))
-
+    println(part1and2(input, 80))
+    println(part1and2(input, 256))
 }
